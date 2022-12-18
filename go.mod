3c2197a811b77608a435cac21b3f4fd7d74e5b9f
ct16k <thciobanu@yahoo.com>
2022-12-18 13:39:28 +0200

    feat: switch to csproto fastmarshal
    Use protoc-gen-fastmarshal from github.com/CrowdStrike/csproto for
    improved performance

--- go.mod
+++ go.mod
@@ -3,9 +3,14 @@ module github.com/mailgun/groupcache/v2
 go 1.19
 
 require (
+	github.com/CrowdStrike/csproto v0.16.0
 	github.com/segmentio/fasthash v1.0.3
 	github.com/sirupsen/logrus v1.9.0
 	google.golang.org/protobuf v1.28.1
 )
 
-require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+require (
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- proto.sh
+++ proto.sh
@@ -9,14 +9,23 @@ PROTO_DIR=testpb
 
 protoc -I=$PROTO_DIR \
     --go_out=$PROTO_DIR \
+    --go_opt=paths=source_relative \
+    --fastmarshal_out=paths=source_relative:$PROTO_DIR \
+    --fastmarshal_opt=apiversion=v2,enableunsafedecode=true \
     $PROTO_DIR/test.proto
 
 PROTO_DIR=groupcachepb
 
 protoc -I=$PROTO_DIR \
     --go_out=$PROTO_DIR \
+    --go_opt=paths=source_relative \
+    --fastmarshal_out=paths=source_relative:$PROTO_DIR \
+    --fastmarshal_opt=apiversion=v2,enableunsafedecode=true \
     $PROTO_DIR/groupcache.proto
 
 protoc -I=$PROTO_DIR \
    --go_out=. \
+   --go_opt=paths=source_relative \
+   --fastmarshal_out=paths=source_relative:. \
+   --fastmarshal_opt=apiversion=v2,enableunsafedecode=true \
     $PROTO_DIR/example.proto
